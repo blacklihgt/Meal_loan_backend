0e73ff6b782716d56cc3c8718d475f606d118618
Roy Kibet <roykibet1@gmail.com>
2026-01-15 15:15:02 +0300

    Change Host name

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
 
   {
-    host: process.env.DB_HOST || 'mysql:8',
+    host: process.env.DB_HOST || 'mysql-8-msqq',
     port: 3306,
     dialect: 'mysql',
 
